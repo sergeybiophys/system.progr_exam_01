bff2b54d87f80cee977f7731f5b079af956c4ecb
sergeybiophys <rove2hati@gmail.com>
2021-10-24 20:26:52 +0300

    added Account Entity

---
+++ MusicShop_Exam/Domain/Entity/Account.cs
@@ -0,0 +1,35 @@
+﻿using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Account
+    {
+        [Unique]
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public bool Status { get; set; }
+
+        public Account() { }
+
+        public Account(string login, string pass,
+            string phone, string email, bool status)
+        {
+            this.Login = login;
+            this.Password = pass;
+            this.Phone = phone;
+            this.Email = email;
+            this.Status = status;
+        }
+    }
+}
---
+++ MusicShop_Exam/Services.Abstract/DTO/AccountDTO.cs
@@ -0,0 +1,37 @@
+﻿using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstract.DTO
+{
+    public class AccountDTO
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        [Unique]
+        public string Phone { get; set; }
+
+        [Unique]
+        public string Email { get; set; }
+
+
+        public string Login
+        {
+            get
+            {
+                return this.Email;
+            }
+            protected set { }
+        }
+        public bool Status { get; set; }
+
+    }
+}
+}
