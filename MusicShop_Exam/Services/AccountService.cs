be03615affd205eff5eea7a8a9501102095a6f1b
sergeybiophys <rove2hati@gmail.com>
2021-10-25 20:04:00 +0300

    point2

---
+++ MusicShop_Exam/Services/AccountService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Repository;
+using Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Services
+{
+    public class AccountService:IAccountService
+    {
+        readonly IUnitOfWork 
+    }
+}
